7a35d47e119ebdeb111671035af92e8e31937f42
tushar <tusharkokane366@gmail.com>
2025-05-28 22:01:19 +0530

    feat: solve two sum problem using HashMap for O(n) efficiency

---
+++ src/main/java/org/example/array/TwoSum.java
@@ -0,0 +1,41 @@
+package org.example.array;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Problem: Two Sum
+ * Author: Tushar Kokane
+ * Date: 2025-05-01
+ *
+ * Time Complexity: O(n)
+ * Space Complexity: O(n)
+ */
+
+public class TwoSum {
+
+    public static int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>(); // value -> index
+
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+
+            if (map.containsKey(complement)) {
+                return new int[] { map.get(complement), i };
+            }
+
+            map.put(nums[i], i);
+        }
+
+        return new int[] {}; // If no solution found
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+
+        int[] result = twoSum(nums, target);
+        System.out.println("Indices: [" + result[0] + ", " + result[1] + "]");
+    }
+}
+
